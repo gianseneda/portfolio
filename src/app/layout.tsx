b541d5e5dae689ca41318e5700c87d2d787e3ec8
Gian Seneda <gian@teuteuf.fr>
2026-02-09 11:50:07 -0300

    Add background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={hostGrotesk.className}>
-      <body className="antialiased relative overflow-x-hidden bg-[#0a0a0a]">
+      <body className="antialiased relative overflow-x-hidden">
         <div className="fixed top-[-20%] left-[-20%] w-[600px] h-[600px] bg-purple-700 blur-3xl opacity-15 rounded-full z-0" />
         <div className="fixed bottom-[-10%] right-[-10%] w-[500px] h-[500px] bg-blue-600 blur-3xl opacity-15 rounded-full z-0" />
         <div className="pt-30">
